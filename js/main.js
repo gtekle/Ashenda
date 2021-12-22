a170c0340eb5ecfd774640db44010c39663fe72a
gtekle <teklegy@gmail.com>
2021-12-22 14:27:11 +0300

    Fix linter errors

--- js/main.js
+++ js/main.js
@@ -27,51 +27,50 @@ mobileNavLinks.forEach((navLink) => {
 
 const speakers = [
   {
-    'title': 'H.E',
-    'fullName': 'Sahle-Work Zewde',
-    'thumbnailImage': './img/president-sahle-work-zewdie.jpg',
-    'currentJob': 'President of Federal Democratic Republic of Ethiopia',
-    'introduction':
+    title: 'H.E',
+    fullName: 'Sahle-Work Zewde',
+    thumbnailImage: './img/president-sahle-work-zewdie.jpg',
+    currentJob: 'President of Federal Democratic Republic of Ethiopia',
+    introduction:
       'Sahle-Work Zewde is an Ethiopian politician and diplomat who is the 5th and current President of Ethiopia since 2018, being the first woman to hold the office. ',
   },
   {
-    'title': 'Artist',
-    'fullName': 'Solomon Bayre',
-    'thumbnailImage': './img/artist-somon-bayre-pic.png',
-    'currentJob': 'Renowned Tigrinya Singer',
-    'introduction':
+    title: 'Artist',
+    fullName: 'Solomon Bayre',
+    thumbnailImage: './img/artist-somon-bayre-pic.png',
+    currentJob: 'Renowned Tigrinya Singer',
+    introduction:
       'Solomon Bayre (Wedi Bayre) is a well-known Ethiopian artist/band. Some of his popular songs are Zebibey, Alekum Do, Tium Zena, Serdo',
   },
   {
-    'title': 'Artist',
-    'fullName': 'Tirhas Tareke',
-    'thumbnailImage': './img/artist-tirhase-tareke-pic.png',
-    'currentJob': 'Renowned Tigrinya Singer',
-    'introduction':
+    title: 'Artist',
+    fullName: 'Tirhas Tareke',
+    thumbnailImage: './img/artist-tirhase-tareke-pic.png',
+    currentJob: 'Renowned Tigrinya Singer',
+    introduction:
       'Born in Eritrea and raised in Adigrat Tirhas Tareke (Kobeley) Awliey started as a circus performer before becoming a renowned traditional Tigrigna musician.',
   },
   {
-    'title': 'Artist',
-    'fullName': 'Nigussie Abadi',
-    'thumbnailImage': './img/artist-niguse-abadi-pic.png',
-    'currentJob': 'Renowned Tigrinya Singer',
-    'introduction':
-      'Solomon Bayre (aka Zemay) is a well-known Ethiopian artist.',
+    title: 'Artist',
+    fullName: 'Nigussie Abadi',
+    thumbnailImage: './img/artist-niguse-abadi-pic.png',
+    currentJob: 'Renowned Tigrinya Singer',
+    introduction: 'Solomon Bayre (aka Zemay) is a well-known Ethiopian artist.',
   },
   {
-    'title': 'Artist',
-    'fullName': 'Eden Gebreselassie',
-    'thumbnailImage': './img/artist-eden-gebreselassie-pic.png',
-    'currentJob': 'Renowned Tigrinya Singer',
-    'introduction':
+    title: 'Artist',
+    fullName: 'Eden Gebreselassie',
+    thumbnailImage: './img/artist-eden-gebreselassie-pic.png',
+    currentJob: 'Renowned Tigrinya Singer',
+    introduction:
       'Eden Gebreselassie is a popular Ethiopian artist, better known with the songs: "Aytneknkni", "Wesene".',
   },
   {
-    'title': 'Artist',
-    'fullName': 'Timnit Welday',
-    'thumbnailImage': './img/artist-timnit-welday-pic.png',
-    'currentJob': 'Renowned Tigrinya Singer',
-    'introduction':
+    title: 'Artist',
+    fullName: 'Timnit Welday',
+    thumbnailImage: './img/artist-timnit-welday-pic.png',
+    currentJob: 'Renowned Tigrinya Singer',
+    introduction:
       'Timnit Welday is a popular Ethiopian artist, better known with the songs: "Ashenda", "Weni", "Wenani".',
   },
 ];
@@ -79,7 +78,7 @@ const speakers = [
 const featuredSpeakersContainer = document.querySelector('.speaker-container');
 
 function generateSpeakers(from, to) {
-  for (let i = from; i < to; i++) {
+  for (let i = from; i < to; i += 1) {
     const speakerHTML = document.createElement('div');
     speakerHTML.className = 'speaker';
     speakerHTML.innerHTML = `<div class="thumbnail-image">
@@ -111,8 +110,6 @@ function generateSpeakers(from, to) {
 }
 
 const btnMore = document.querySelector('.btn-more');
-const partners = document.querySelector('#index-partners');
-const footer = document.querySelector('#index-footer');
 
 function isScreenSmall() {
   if (window.screen.width < 768) {
@@ -148,11 +145,11 @@ window.addEventListener('load', () => {
 btnMore.addEventListener('click', () => {
   if (!btnMore.classList.contains('less')) {
     btnMore.classList.add('less');
-    btnMore.innerHTML = `Less <i class="fas fa-angle-up"></i>`;
+    btnMore.innerHTML = 'Less <i class="fas fa-angle-up"></i>';
     generateSpeakers(NUMBER_OF_SPEAKERS_ON_LOAD, speakers.length);
   } else {
     btnMore.classList.remove('less');
-    btnMore.innerHTML = `More<i class="fas fa-angle-down"></i>`;
+    btnMore.innerHTML = 'More <i class="fas fa-angle-down"></i>';
     featuredSpeakersContainer.innerHTML = '';
     generateSpeakers(0, NUMBER_OF_SPEAKERS_ON_LOAD);
   }
